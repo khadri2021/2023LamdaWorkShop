d573f6e5b7fcfa9e0013f8a9978866f98fcea942
Jeelan9686 <shaikjeelan533526@gmail.com>
2023-02-16 23:38:47 +0530

    commited

--- src/Testpojo.java
+++ src/Testpojo.java
@@ -1,20 +1,22 @@
 
 public class Testpojo {
-String name;
-int id;
+	String name;
+	int id;
 
-public void setName(String name) {
-	this.name = name;
-}
-public void setId(int id) {
-	this.id = id;
-}
-public String getName() {
-	return name;
-}
-public int getId() {
-	return id;
-}
+	public void setName(String name) {
+		this.name = name;
+	}
 
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getId() {
+		return id;
+	}
 
 }
