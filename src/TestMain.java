206cb962e680410e1f6209e07b5c7183500701f9
Jeelan9686 <shaikjeelan533526@gmail.com>
2023-02-16 23:26:53 +0530

    commited successfully

---
+++ src/TestMain.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.function.Predicate;
+
+public class TestMain {
+	static Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		System.out.println("WELCOME TO TEST MAIN....");
+		System.out.println("");
+
+		System.out.println("ENTER YOUR NAME ");
+		String name = sc.next();
+		System.out.println("ENTER YOUR ID ");
+		int id = sc.nextInt();
+
+		Testpojo tp = new Testpojo();
+		tp.setName(name);
+		tp.setId(id);
+		Predicate<String> pr = (str) -> {
+			return !str.isEmpty();
+		};
+		boolean name1 = pr.test(name);
+		System.out.println("your NAME is " + name1);
+
+		Predicate<Integer> pr1 = (i) -> {
+			return i > 0;
+		};
+		boolean result1 = pr1.test(id);
+		System.out.println("your ID IS " + result1);
+
+	}
+
+}
---
+++ src/Testpojo.java
@@ -0,0 +1,20 @@
+
+public class Testpojo {
+String name;
+int id;
+
+public void setName(String name) {
+	this.name = name;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public int getId() {
+	return id;
+}
+
+
+}
