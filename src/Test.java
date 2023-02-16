f52c2fb00384e738607c95bb955a56cb1b6d783e
Babjanshake <Babjanshake@gmail.com>
2023-02-16 23:16:09 +0530

    initial changes

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+import java.util.function.Predicate;
 
 public class Test {
+	private static Scanner sc;
+	static {
+		sc = new Scanner(System.in);
+	}
+
 	public static void main(String[] args) {
-		System.out.println("change");
+		Predicate<Integer> predicate = (age) -> {
+			if (age >= 18) {
+				return true;
+			} else {
+				return false;
+			}
+		};
+		System.out.println("Please Enter Your Age : ");
+
+		boolean test = predicate.test(sc.nextInt());
+		if (test) {
+			System.out.println("You Are Eligible For Vote. Because Your Age Is Greater Than 17 Years.");
+		} else {
+			System.out.println("You Are Not Eligible For Vote. Because Your Age Is Less Than 18 Years.");
+		}
 	}
 }
