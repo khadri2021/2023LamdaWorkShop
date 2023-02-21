a9265edba75cf7175a303096a920ab7e8fa9c54a
Babjanshake <Babjanshake@gmail.com>
2023-02-21 21:38:00 +0530

    Requirement Based Function Changes1

--- src/com/khadri/functional/maths/FunctionTest2.java
+++ src/com/khadri/functional/maths/FunctionTest2.java
@@ -6,12 +6,12 @@ import java.util.function.Function;
 
 public class FunctionTest2 {
 	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<Integer>();
+		List<Integer> list = new ArrayList<>();
 		list.add(10);
 		list.add(20);
 		list.add(30);
 		list.add(40);
-		System.out.println("The Numbers Which Are Used In Addition : "+list.toString());
+		System.out.println("The Numbers Which Are Used In Addition : "+list);
 		Function<List<Integer>, Integer> fun = list1 -> {
 			int sum = 0;
 			for (Integer integer : list1) {
